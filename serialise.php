7c13321342a27b11d9d9494260acb3f7bbfb7cdd
sagardrc <sagar.panchal@drcsystems.com>
2022-11-09 14:36:53 +0530

    added serialise.php

---
+++ serialise.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "form_key=zxScQbxzpfQEDCOF&limit=20&page=1&entity_id=&name=&email=&Telephone=&billing_postcode=&billing_country_id=&billing_regione=&store_name=&website_name=&limit=20&page=1&entity_id=&name=&sku=&price%5Bfrom%5D=&price%5Bto%5D=&in_products=&item%5B230%5D%5Bqty%5D=1&item%5B230%5D%5Buse_discount%5D=1&item%5B230%5D%5Baction%5D=&item%5B231%5D%5Bqty%5D=1&item%5B231%5D%5Buse_discount%5D=1&item%5B231%5D%5Baction%5D=&item%5B232%5D%5Bqty%5D=1&item%5B232%5D%5Buse_discount%5D=1&item%5B232%5D%5Baction%5D=&coupon_code=&order%5Baccount%5D%5Bgroup_id%5D=1&order%5Baccount%5D%5Bemail%5D=kshama.parmar%40drcsystems.com&order%5Bbilling_address%5D%5Bcustomer_address_id%5D=&order%5Bbilling_address%5D%5Bprefix%5D=0&order%5Bbilling_address%5D%5Bfirstname%5D=&order%5Bbilling_address%5D%5Bmiddlename%5D=&order%5Bbilling_address%5D%5Blastname%5D=&order%5Bbilling_address%5D%5Bsuffix%5D=&order%5Bbilling_address%5D%5Bcompany%5D=&order%5Bbilling_address%5D%5Bstreet%5D%5B0%5D=&order%5Bbilling_address%5D%5Bstreet%5D%5B1%5D=&order%5Bbilling_address%5D%5Bstreet%5D%5B2%5D=&order%5Bbilling_address%5D%5Bcountry_id%5D=US&order%5Bbilling_address%5D%5Bregion%5D=&order%5Bbilling_address%5D%5Bregion_id%5D=&order%5Bbilling_address%5D%5Bcity%5D=&order%5Bbilling_address%5D%5Bpostcode%5D=&order%5Bbilling_address%5D%5Btelephone%5D=&order%5Bbilling_address%5D%5Bfax%5D=&order%5Bbilling_address%5D%5Bvat_id%5D=&shipping_same_as_billing=on&payment%5Bmethod%5D=checkmo&order%5Bhas_shipping%5D=&order%5Bcomment%5D%5Bcustomer_note%5D=&order%5Bcomment%5D%5Bcustomer_note_notify%5D=1&order%5Bsend_confirmation%5D=1&order%5Bcurrency%5D=USD";
+
+echo "<pre>";
+print_r(explode("&",$txt));
+
+?>
+
+</body>
+</html>
